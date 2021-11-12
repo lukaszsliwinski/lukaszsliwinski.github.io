90ed0929c6222a68ef1c3b298972b2819b1fc2bb
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2021-11-12 22:05:17 +0100

    Organize js file. Write own showHideProject() function based on copied jQuery version

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-let about = document.getElementById('about');
-let main = document.getElementById('main');
+const about = document.getElementById('about');
+const main = document.getElementById('main');
 
-function showHide() {
+const sections = document.querySelectorAll('.section-header');
+const navBtns = document.querySelectorAll('.nav-btn');
+
+
+// highlight navigation menu
+window.addEventListener('scroll', () => {
+    let currentSection = '';
+    sections.forEach( section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (scrollY >= (sectionTop - sectionHeight / 3)) {
+            currentSection = section.getAttribute('id');
+        };
+    });
+    navBtns.forEach( btn => {
+        btn.classList.remove('active');
+        if (btn.classList.contains(currentSection)) {
+            btn.classList.add('active');
+        };
+    });
+});
+
+
+// function for showing and hiding main information area
+function showHideAbout() {
     if (about_btn.innerHTML == '&lt;&lt;') {
         about_btn.innerHTML = '&gt;&gt;';
         about.classList.remove('visible');
@@ -18,36 +42,22 @@ function showHide() {
 };
 
 
-jQuery('.project .project-name').click(function(){ 
-	if ( jQuery(this).parent().hasClass('extended') ) 
-	{
-		jQuery('.project').removeClass('extended');
-        jQuery(this).children().html('&#129095;');
-	} else {
-        jQuery('.project').removeClass('extended');
-        jQuery('.extend-btn').html('&#129095;');
-        jQuery(this).parent().addClass('extended');
-        jQuery(this).children().html('&#129093;');
-	}
-});
-
-// highlight navigation menu
-const sections = document.querySelectorAll('.section-header');
-const navLi = document.querySelectorAll('.header a');
-
-window.addEventListener('scroll', () => {
-    let current = '';
-    sections.forEach( section => {
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.clientHeight;
-        if (scrollY >= (sectionTop - sectionHeight / 3)) {
-            current = section.getAttribute('id');
-        };
-    });
-    navLi.forEach( a => {
-        a.classList.remove('active');
-        if (a.classList.contains(current)) {
-            a.classList.add('active');
-        };
-    });
-});
+// function for showing and hiding project details
+function showHideProject(id) {
+    let project = document.getElementById(id);
+    if (project.parentElement.classList.contains('extended')) {
+        Array.from(document.getElementsByClassName('project')).forEach( proj => {
+            proj.classList.remove('extended');
+        });
+        project.firstElementChild.innerHTML = '&#129095;';
+    } else {
+        Array.from(document.getElementsByClassName('project')).forEach( proj => {
+            proj.classList.remove('extended');
+        });
+        Array.from(document.getElementsByClassName('extend-btn')).forEach( btn => {
+            btn.innerHTML = '&#129095;';
+        });
+        project.parentElement.classList.add('extended');
+        project.firstElementChild.innerHTML = '&#129093;';
+    };
+};
