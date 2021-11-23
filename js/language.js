61a8163bbde2cca14e2ca673c9fb09b0da20a644
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2021-11-23 20:47:14 +0100

    Translate about section

--- js/language.js
+++ js/language.js
@@ -20,9 +20,15 @@ const plProjHeaders = {
 };
 
 // Define polish text content
-
-
-
+const plContent = {
+    aboutContent: `&emsp;&emsp;&emsp;Jestem początkującym programistą stawiającym pierwsze kroki w&nbsp;tworzeniu aplikacji 
+    internetowych. W&nbsp;trakcie samodzielnej nauki poznałem podstawową składnię takich języków jak Python 
+    oraz JavaScript. W&nbsp;ramach ćwiczeń praktycznych utworzyłem kilka aplikacji poznając podstawy tworzenia 
+    backend&#8209;u i&nbsp;frontend&#8209;u aplikacji, sposoby komunikacji między nimi, bazy danych, 
+    tworzenie szablonów responsywnych i&nbsp;system kontroli wersji. Poniżej przedstawiam kilka efektów mojej 
+    dotychczasowej nauki. Poszukuję nowych wyzwań, zawodowych i&nbsp;hobbystycznych, które umożliwią mi dalszy 
+    rozwój i&nbsp;poszerzenie praktycznej wiedzy.`
+};
 
 // Define english document headers
 const engHeaders = {
@@ -44,7 +50,15 @@ const engProjHeaders = {
 };
 
 // Define english text content
-
+const engContent = {
+    aboutContent: `&emsp;&emsp;&emsp;I&nbsp;am a&nbsp;beginner programmer taking the first steps in creating web applications. 
+    During self&#8209;study, I&nbsp;learned the basic syntax of such languages ​​as Python and JavaScript. 
+    As part of the practical exercises, I&nbsp;created several applications, learning the basics of 
+    creating an application backend and frontend, methods of communication between them, 
+    databases, creating responsive templates and the version control system. Below I&nbsp;present 
+    some effects of my previous learning. I&nbsp;am looking for new professional and hobby challenges 
+    which will enable me to further develop and expand my practical knowledge.`
+};
 
 
 
@@ -56,20 +70,27 @@ const engProjHeaders = {
 // Define function to set language
 function setLanguage() {
     if (lang == 'pl') {
-        for (const [key, value] of Object.entries(plHeaders)) {
-            document.getElementById(key).innerHTML = value;
+        // Set polish document headers and text content
+        for (const object of [plHeaders, plContent]) {
+            for (const [key, value] of Object.entries(object)) {
+                document.getElementById(key).innerHTML = value;
+            };
         };
 
+        // Set polish project details headers (the same value for different ids)
         for (const [key, value] of Object.entries(plProjHeaders)) {
             for (i=1; i<6; i++) {
                 document.getElementById(key + String(i)).innerHTML = value
             };
         };
     } else {
-        for (const [key, value] of Object.entries(engHeaders)) {
-            document.getElementById(key).innerHTML = value;
+        // Set english document headers and text content
+        for (const object of [engHeaders, engContent]) {
+            for (const [key, value] of Object.entries(object)) {
+                document.getElementById(key).innerHTML = value;
+            };
         };
-
+        // Set english project details headers (the same value for different ids)
         for (const [key, value] of Object.entries(engProjHeaders)) {
             for (i=1; i<6; i++) {
                 document.getElementById(key + String(i)).innerHTML = value
@@ -79,12 +100,12 @@ function setLanguage() {
 };
 
 
-// Set initial statement
+// Initial statement
 let lang = 'pl'
 setLanguage();
 
 
-// Function to change lang value
+// Function to change lang value - assign to flag btns
 changeLanguage = (language) => {
     lang = language;
     setLanguage();
